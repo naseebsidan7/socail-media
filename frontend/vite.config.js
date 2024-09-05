c79f5559a832ecbad7145ea0332c4a06d1579b52
naseebsidan07 <naseebsidan6@gmail.com>
2024-09-05 21:53:02 +0530

    vite.config file updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 server:{
-     port: 5173,
      proxy: {
         '/api':{
           target:'https://socail-media-9yli.onrender.com',
-          changeOrigin:true
+          secure: false
         }
      }
   },
